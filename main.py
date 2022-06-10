91354b745ed35ffb27b17e24abf987d717502c8e
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-06-10 08:11:04 +0545

    fastapi init

---
+++ main.py
@@ -0,0 +1,13 @@
+from imp import reload
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
