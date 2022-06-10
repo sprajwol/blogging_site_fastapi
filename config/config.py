d2f570725ed56fdc3f9d8b09feed5b43f8526c21
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-06-10 12:40:36 +0545

    access refresh token expiry

--- config/config.py
+++ config/config.py
@@ -1,8 +1,11 @@
 from pydantic import BaseSettings
+from datetime import timedelta
 
 
 class JWTSettings(BaseSettings):
     authjwt_secret_key: str = "secret"
+    access_expires: int = timedelta(minutes=15)
+    refresh_expires: int = timedelta(days=30)
 
     class Config:
         env_file = '.env'
